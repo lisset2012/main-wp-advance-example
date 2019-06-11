43092867f61728254adb3f208b6dd29d1fd26569
lisset2012 <lisset2012@gmail.com>
2019-06-11 18:38:07 -0400

    Add files via upload

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Main WP Advance Example Theme Customizer
+ *
+ * @package Main_WP_Advance_Example
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function main_wp_advance_example_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'main_wp_advance_example_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'main_wp_advance_example_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'main_wp_advance_example_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function main_wp_advance_example_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function main_wp_advance_example_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function main_wp_advance_example_customize_preview_js() {
+	wp_enqueue_script( 'main-wp-advance-example-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'main_wp_advance_example_customize_preview_js' );
